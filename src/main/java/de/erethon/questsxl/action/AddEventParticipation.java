29c4b0cc3aefb59e1cc22c0b474a27d079052722
Malfrador <malfrador@gmail.com>
2022-07-26 23:54:45 +0200

    Load AddEventParticipation action event IDs after events.

--- src/main/java/de/erethon/questsxl/action/AddEventParticipation.java
+++ src/main/java/de/erethon/questsxl/action/AddEventParticipation.java
@@ -11,12 +11,13 @@ public class AddEventParticipation extends QBaseAction {
     QuestsXL plugin = QuestsXL.getInstance();
     QPlayerCache playerCache = plugin.getPlayerCache();
 
-    private QEvent event;
+    private String id;
     private int amount;
 
     @Override
     public void play(QPlayer player) {
         if (!conditions(player)) return;
+        QEvent event = plugin.getEventManager().getByID(id);
         event.participate(player, amount);
         onFinish(player);
     }
@@ -28,14 +29,14 @@ public class AddEventParticipation extends QBaseAction {
 
     @Override
     public void load(String[] msg) {
-        event = plugin.getEventManager().getByID(msg[0]);
+        id = msg[0];
         amount = Integer.parseInt(msg[1]);
     }
 
     @Override
     public void load(ConfigurationSection section) {
         super.load(section);
-        event =  plugin.getEventManager().getByID(section.getString("message"));
+        id =  section.getString("id");
         amount = section.getInt("amount");
     }
 }
